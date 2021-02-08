9787d0d293caf6e985079ce432f1b885cada08c8
挽风 <73214348+byclear@users.noreply.github.com>
2021-02-08 16:08:41 +0800

    Update 开始.py

--- 开始.py
+++ 开始.py
@@ -13,11 +13,6 @@ def msgdata():
                     print("公众号消息")
                 if data[2].find("@chatroom") == -1 and data[2][0:3].find("gh_") == -1: #排除 群里发的消息 公众号收到的消息
                     print("私人消息")
-                    #下面是AI调用实例
-                    a=requests.get("http://ai.wfzc.vip/"+str(data[6])+"/")
-                    text = a.text
-                    if text=='<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">欢迎来到挽风智障AI <br/>请在连接携带要问<br>如 http://ai.wfzc.vip/你在干嘛啊/ 访问接口':
-                        text="不知道你在说什么"
                     fxx(data[2], text[0:100])#发送消息 data[2]=微信id  text[0:100]=请求后的消息
 threading.Thread(target=msgdata).start()#多线程收消息 处理
 input("启动微信 作者qq：172502080")
