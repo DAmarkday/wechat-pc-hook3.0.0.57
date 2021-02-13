d65e69c12cf5a1050b52ca8d6392c4a51343ec85
挽风 <73214348+byclear@users.noreply.github.com>
2021-02-13 13:35:55 +0800

    Update 开始.py

--- 开始.py
+++ 开始.py
@@ -1,12 +1,11 @@
-import threading,requests  # 导入两个模块 安装。pip install requests
-from ks import *    #导入模块ks 无需insatll
-# 监听消息实例 通过消息来判断
-def msgdata():
+import threading                     # 导入模块 3.7自带 threading 无需安装。
+from ks import *                     #导入已经在文件夹的ks.pyd 模块 ks 无需insatll
+def msgdata():                       # 监听消息实例 通过消息来判断
     msger = jtxx()                   #回调内部方法。如pycharm报红色字体。可以忽视。照样运行
     for data in msger:               #打印方法
         print(data)                  #这里回调所有消息。可以直接使用
         if data[0]=="msgdp":         #消息内容
-            if data[5]=="0":         # 1代表发送消息 0代表收到消息
+            if data[5]=="0":         #1代表发送消息 0代表收到消息
                 if data[2].find("@chatroom") != -1:  # 群里发的消息
                     print("群里发的消息")
                 if data[2][0:3].find("gh_") != -1:   #公众号收到的消息
@@ -16,4 +15,3 @@ def msgdata():
                     fxx(data[2], "发送成功")#发送消息 data[2]=微信id  text[0:100]=请求后的消息
 threading.Thread(target=msgdata).start()#多线程收消息 处理
 input("启动微信")
-
